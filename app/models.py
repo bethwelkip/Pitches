e19fac40b8387c1c8c3e7cbab91ec3f21e280395
Bethwel Kiplimo <bethwelkiplimo@Bethwels-MacBook-Pro.local>
2020-10-30 01:30:58 -0400

    create User model

---
+++ app/models.py
@@ -0,0 +1,28 @@
+from . import db, login_manager
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    user_id = db.Column(db.Integer, primary_key =True)
+    username = db.Column(db.String(255))
+    password = db.Column(db.String())
+    email = db.Column(db.String()) 
+    def __repr__(self):
+        return f'{self.username}'
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @classmethod
+    def password_hash(self, password):
+        self.password = generate_password_hash(password)
+        return password
+    
+    def verify_password(self, passw):
+        return check_password_hash(self.password,passw)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
